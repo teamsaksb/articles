0645670ae0eb1d2f6584d55ce7969ae78118d127
pica84 <pica84@nate.com>
2024-10-14 15:33:43 +0900

    Article Entity 생성 및 데이터 세팅완료

---
+++ src/main/java/com/my/articles/entity/Articles.java
@@ -0,0 +1,14 @@
+package com.my.articles.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Articles {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(nullable = false, length = 50)
+    private String title;
+    @Column(nullable = false, length = 1000)
+    private String content;
+}
